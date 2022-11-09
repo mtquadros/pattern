d5850d9819828d959aa6689becc24b6167724aa3
Maurício Quadros <mauriciotq@gmail.com>
2022-11-09 18:13:48 -0300

    This implementation of inheritence do not work to instanciate subclasses of Singleton because A::Init() will always call A::_Init() even though B::_Init() overrides A::_Init().

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+﻿// pattern.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "pattern.h"
+
+using namespace std;
+
+int main()
+{
+	B* b;
+
+	b = static_cast<B*>(B::Init());
+
+	delete b;
+	
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ pattern.h
@@ -0,0 +1,55 @@
+﻿// pattern.h: arquivo de inclusão para arquivos de inclusão padrão do sistema,
+// ou arquivos de inclusão específicos a um projeto.
+
+#pragma once
+
+#include <iostream>
+
+class A {
+private:
+	int _a;
+
+protected:
+	static A* singleton;
+	A() { _a = 0; printA(); }
+	A(int n) { _a = n; printA(); }
+	static A* _Init() {
+		//retorna a instancia desejada
+		std::cout << "_Init of A \n";
+		return new A();
+	}
+public:
+	void printA() { std::cout << "A == " << _a << std::endl; }
+
+	//deve ser sobrecarregada
+	~A() { std::cout << "Destroi A" << std::endl; }
+
+	static A* Init() {
+		if (singleton == nullptr)
+			singleton = _Init();
+
+		return singleton;
+	}
+
+};
+A* A::singleton = nullptr;
+
+class B : public A {
+private:
+	int _b;
+
+	B() { _b = 0; printB(); }
+	B(int n) { _b = n; printB(); }
+
+public:
+	void printB() { std::cout << "B == " << _b << std::endl; }
+	~B() { std::cout << "Destroy B" << std::endl; }
+
+protected:
+	static B* _Init() {
+		std::cout << "_Init() of B\n";
+		return new B();
+	}
+};
+
+// TODO: Referencie os cabeçalhos adicionais de que seu programa precisa aqui.
